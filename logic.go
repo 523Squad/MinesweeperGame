c919f40a7bd09f24be7c989ac7a0278b0a81a988
Max Ratoshniuk <maksratosh@gmail.com>
2016-10-05 00:23:20 +0300

    added file with structs and methods

---
+++ logic.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"math/rand"
+)
+
+const EASY_LVL_DIM = 9
+const EASY_LVL_BOMBS = 10
+const MIDDLE_LVL_DIM = 16
+const MIDDLE_LVL_BOMBS = 40
+const HARD_LVL_DIM = 30
+const HARD_LVL_BOMBS = 116
+
+type Point struct {
+	touched bool
+	isBomb bool
+	bombsNumber int
+}
+
+type Board struct {
+	bombsNumber int
+	dimension int
+	field [][]*Point
+	gameOver bool
+}
+
+func (p *Point) toString() string{
+	return " " +  strconv.FormatBool(p.isBomb)
+}
+
+func (b *Board) setBoard(n int) {
+	for i := 0; i < n; i++ {
+		row := []*Point{}
+		for j := 0; j < n; j++ {
+			row = append(row, new(Point))
+		}
+		b.field = append(b.field, row)
+	}
+	b.setBombs()
+}
+
+func (b *Board) setBombs() {
+	count := b.bombsNumber
+	for count > 0 {
+		if (b.field[rand.Intn(b.dimension)][rand.Intn(b.dimension)].isBomb == false) {
+			b.field[rand.Intn(b.dimension)][rand.Intn(b.dimension)].isBomb = true
+			count -= 1
+		}
+	}
+	//c := 0
+	//for i := 0; i < b.dimension; i++ {
+	//	for j := 0; j < b.dimension; j++ {
+	//		if (b.field[i][j].isBomb == true) {
+	//			fmt.Println(b.field[i][j].toString() + "		" + strconv.Itoa(c))
+	//			c++
+	//		} else {
+	//			fmt.Println(b.field[i][j].toString())
+	//		}
+	//
+	//
+	//	}
+	//}
+}
+
+func main() {
+	p := Board{
+		dimension:EASY_LVL_DIM,
+		bombsNumber:EASY_LVL_BOMBS,
+	}
+	p.setBoard(p.dimension)
+	fmt.Println(strconv.FormatBool(p.gameOver))
+
+}
