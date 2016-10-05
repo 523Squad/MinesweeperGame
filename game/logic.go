9bee3741153956109bc244170b2c6ce0a3be8294
Max Ratoshniuk <maksratosh@gmail.com>
2016-10-05 19:27:36 +0300

    Update logic.go

--- game/logic.go
+++ game/logic.go
@@ -4,21 +4,22 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"time"
 )
 
-// TODO: Rename constants.
-// see: https://stackoverflow.com/questions/22688906/go-naming-conventions-for-const
-const EASY_LVL_DIM = 9
-const EASY_LVL_BOMBS = 10
-const MIDDLE_LVL_DIM = 16
-const MIDDLE_LVL_BOMBS = 40
-const HARD_LVL_DIM = 30
-const HARD_LVL_BOMBS = 116
+
+const EasyLvlDimension = 9
+const EasyLvlBombsNumber = 10
+const MediumLvlDimension = 16
+const MediumLvlBombsNumber = 40
+const HardLvlDimension = 30
+const HardLvlBombsNumber = 116
 
 type point struct {
-	touched     bool
-	isBomb      bool
-	bombsNumber int
+	isOpened          bool
+	hasFlag           bool
+	hasMine           bool
+	neighborMineCount int
 }
 
 // Board represents minesweeper board.
@@ -30,7 +31,7 @@ type Board struct {
 }
 
 func (p *point) toString() string {
-	return " " + strconv.FormatBool(p.isBomb)
+	return " " + strconv.FormatBool(p.hasMine) + " neighbours " + strconv.Itoa(p.neighborMineCount)
 }
 
 func (b *Board) setBoard(n int) {
@@ -42,31 +43,40 @@ func (b *Board) setBoard(n int) {
 		b.field = append(b.field, row)
 	}
 	b.setBombs()
+	b.setBombsNeighbours()
 }
 
 func (b *Board) setBombs() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	count := b.bombsNumber
 	for count > 0 {
 		x := rand.Intn(b.dimension)
 		y := rand.Intn(b.dimension)
-		if b.field[x][y].isBomb == false {
-			b.field[x][y].isBomb = true
+		if b.field[x][y].hasMine == false {
+			b.field[x][y].hasMine = true
 			count--
 		}
 	}
-	//c := 0
-	//for i := 0; i < b.dimension; i++ {
-	//	for j := 0; j < b.dimension; j++ {
-	//		if (b.field[i][j].isBomb == true) {
-	//			fmt.Println(b.field[i][j].toString() + "		" + strconv.Itoa(c))
-	//			c++
-	//		} else {
-	//			fmt.Println(b.field[i][j].toString())
-	//		}
-	//
-	//
-	//	}
-	//}
+}
+
+func (b *Board) setBombsNeighbours() {
+	for i := 0; i < b.dimension; i++ {
+		for j := 0; j < b.dimension; j++ {
+			coords := []int{-1, 0, 1}
+			for _, ki := range coords {
+				for _, kj := range coords {
+					if (ki == 0 && kj == 0) {
+						continue
+					} else if (((ki + i >= 0) && (ki + i < b.dimension)) &&
+						((kj + j >= 0) && (kj + j < b.dimension))) {
+						if (b.field[ki + i][kj + j].hasMine == true) {
+							b.field[i][j].neighborMineCount++
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 // continue tells whether we should keep playing.
@@ -82,3 +92,5 @@ func (b *Board) initGame() {
 	fmt.Println(strconv.FormatBool(b.gameOver))
 
 }
+
+
